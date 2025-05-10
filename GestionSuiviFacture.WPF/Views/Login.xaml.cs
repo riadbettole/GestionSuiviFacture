e84fc82be5fc18e2a25c0a1bb50cd0fdbbe38949
riadbettole <118698087+riadbettole@users.noreply.github.com>
2025-05-10 15:10:52 +0100

    Added first attempt Login Interface

---
+++ GestionSuiviFacture.WPF/Views/Login.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GestionSuiviFacture.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
