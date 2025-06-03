6622df7c7aa85b68ef4e2c5b31f632991e3f78c6
riadbettole <118698087+riadbettole@users.noreply.github.com>
2025-06-03 09:53:27 +0100

    quick fix

---
+++ GestionSuiviFacture.WPF/Converters/StringEqualsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GestionSuiviFacture.WPF.Converters
+{
+    public class StringEqualsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.ToString() == parameter.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
